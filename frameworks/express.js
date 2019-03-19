1da60f2e3b7ed9f3e079f631975550e72e8ec3c4
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2019-03-19 05:13:29 -0700

    frameworks/express - use sendFile()
    - sendfile is deprecated.

--- frameworks/express.js
+++ frameworks/express.js
@@ -414,7 +414,7 @@ exports.init = function (options) {
   //==========================================================================
   app.get('/', function home (req, res) {
     // load the single view file (angular will handle the page changes on the front-end)
-    res.sendfile('index.html');
+    res.sendFile('index.html');
   });
 
   app.use(function (req, res) {
